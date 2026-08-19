d0c53d3e8370a1dfe09fd41a4fb207672ea5b98a
krisjiang <krisjiang@didiglobal.com>
2026-08-19 17:31:16 +0800

    leetcode

--- didi/15.py
+++ didi/15.py
@@ -2,3 +2,12 @@ class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
         nums.sort()
         res = []
+        for i in range(len(nums)-2):
+            left, right = i+1, len(nums)-1
+            if nums[i] + nums[left] + nums[right] == 0:
+                res.append([nums[i], nums[left], nums[right]])
+            elif nums[i] + nums[left] + nums[right] < 0:
+                left += 1
+            else:
+                right -= 1
+        return res
