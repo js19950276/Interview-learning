038cd19d4cdd7e842c2d7fd0b1996ca96d7a460a
krisjiang <krisjiang@didiglobal.com>
2026-08-19 17:35:35 +0800

    leetcode

--- didi/15.py
+++ didi/15.py
@@ -4,10 +4,19 @@ class Solution:
         res = []
         for i in range(len(nums)-2):
             left, right = i+1, len(nums)-1
-            if nums[i] + nums[left] + nums[right] == 0:
-                res.append([nums[i], nums[left], nums[right]])
-            elif nums[i] + nums[left] + nums[right] < 0:
-                left += 1
-            else:
-                right -= 1
+            while left < right:
+                if nums[i] + nums[left] + nums[right] == 0:
+                    if [nums[i], nums[left], nums[right]] not in res:
+                        res.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                elif nums[i] + nums[left] + nums[right] < 0:
+                    left += 1
+                else:
+                    right -= 1
         return res
+
+if __name__ == '__main__':
+    nums = [-1, 0, 1, 2, -1, -4]
+    a = Solution()
+    print(a.threeSum(nums))
