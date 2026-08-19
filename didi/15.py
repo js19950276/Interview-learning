01f809d3ed1c148e997e5e4d169d3b84fda72e1c
krisjiang <krisjiang@didiglobal.com>
2026-08-19 17:45:30 +0800

    leetcode

--- didi/15.py
+++ didi/15.py
@@ -3,6 +3,8 @@ class Solution:
         nums.sort()
         res = []
         for i in range(len(nums)-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
             left, right = i+1, len(nums)-1
             while left < right:
                 if nums[i] + nums[left] + nums[right] == 0:
